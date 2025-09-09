84d276ae61f44ef7a0d1b5840cbda603d420ab1c
ceoacrvn <ceo@acrvn.site>
2025-09-10 06:14:06 +0700

    Add files via upload

---
+++ index.jsx
@@ -0,0 +1,8 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+
+function App() {
+  return <h1>SuperPOS Clone Connected to Supabase 🚀</h1>
+}
+
+ReactDOM.createRoot(document.getElementById('root')).render(<App />)
